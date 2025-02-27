dba3d790bf78ad0881c856aa47323ef04e7b3c14
Aniket Rathore <aniketrathore911@gmail.com>
2025-02-27 23:40:25 +0530

    Bean Creation Using Stereotype Annotation

--- src/main/java/com/springboot/beans/SpringBoot/Apple.java
+++ src/main/java/com/springboot/beans/SpringBoot/Apple.java
@@ -1,4 +1,12 @@
 package com.springboot.beans.SpringBoot;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class Apple {
+
+    public void eatApple()
+    {
+        System.out.println("Eat Apple ");
+    }
 }
