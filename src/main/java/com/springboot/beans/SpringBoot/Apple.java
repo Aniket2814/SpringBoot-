e692d04e890a27c17e05b1cb062193cebfa1d2e6
Aniket Rathore <aniketrathore911@gmail.com>
2025-02-27 23:59:03 +0530

    Bean Life Cycle

--- src/main/java/com/springboot/beans/SpringBoot/Apple.java
+++ src/main/java/com/springboot/beans/SpringBoot/Apple.java
@@ -1,5 +1,7 @@
 package com.springboot.beans.SpringBoot;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.springframework.stereotype.Component;
 
 
@@ -8,5 +10,18 @@ public class Apple {
     public void eatApple()
     {
         System.out.println("Eat Apple ");
+    }
+
+    @PostConstruct
+    public void show1()
+    {
+
+        System.out.println("Bean is Created ");
+    }
+
+    @PreDestroy
+    public void show2()
+    {
+        System.out.println("Bean is Destroyed");
     }
 }
