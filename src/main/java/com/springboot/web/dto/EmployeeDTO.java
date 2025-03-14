9264847700de4bad18ef0fc7b572655fe6ac997c
Aniket Rathore <aniketrathore911@gmail.com>
2025-03-14 18:56:02 +0530

    Fixing isActive Field With help of @JsonProperty

--- src/main/java/com/springboot/web/dto/EmployeeDTO.java
+++ src/main/java/com/springboot/web/dto/EmployeeDTO.java
@@ -1,9 +1,17 @@
 package com.springboot.web.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.LocalDate;
 
+@Getter
+@AllArgsConstructor
+@Setter
+@NoArgsConstructor
 public class EmployeeDTO {
 
     private Long id;
@@ -14,64 +22,5 @@ public class EmployeeDTO {
 @JsonProperty("isActive")
     private Boolean isActive;
 
-    public LocalDate getDateofJoining() {
-        return dateofJoining;
-    }
 
-    public void setDateofJoining(LocalDate dateofJoining) {
-        this.dateofJoining = dateofJoining;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public Boolean getActive() {
-        return isActive;
-    }
-
-    public void setActive(Boolean active) {
-        isActive = active;
-    }
-
-    public  EmployeeDTO()
-    {
-
-    }
-    public EmployeeDTO(Long id, String name, String email, Integer age, LocalDate dateofJoining, Boolean isActive) {
-        this.id = id;
-        this.name = name;
-        this.email = email;
-        this.age = age;
-        this.dateofJoining = dateofJoining;
-        this.isActive = isActive;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
 }
